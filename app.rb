a21c5182c77585dc3fafd6818287fb8e3ff2038d
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2024-04-14 19:06:03 +0000

    update to drill down pages

--- app.rb
+++ app.rb
@@ -26,15 +26,29 @@ end
 
 
 
-
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
+  # build the API url, including the API key in the query string
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
-  
-  # some more code to parse the URL and render a view template
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  @symbols = parsed_data["currencies"].keys
+
+  @symbols_list = @symbols.map{|currency| "<li><a href=\"/#{@original_currency}/#{currency}\">Convert 1 #{@original_currency} to #{currency}...</li>"}.join(" ")
+
+  erb(:individual)
 
 end
+
 
 get("/:from_currency/:to_currency") do
   @original_currency = params.fetch("from_currency")
@@ -43,5 +57,15 @@ get("/:from_currency/:to_currency") do
   api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
   
   # some more code to parse the URL and render a view template
+  raw_data = HTTP.get(api_url)
 
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  @result = parsed_data["result"]
+
+  erb(:rate)
 end
